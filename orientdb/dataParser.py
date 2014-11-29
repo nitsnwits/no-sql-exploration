44973a0d43eb897bc56295690d10d94f1a7a6451
Neeraj Sharma <nitsnwits@gmail.com>
2014-11-29 00:30:31 -0800

    Working data parser

--- orientdb/dataParser.py
+++ orientdb/dataParser.py
@@ -7,6 +7,7 @@ bookCount = 0
 titleCount = 0
 authorCount = 0
 releaseDateCount = 0
+languageCount = 0
 bookName = ""
 for book in zp.infolist():
 	#print book.filename
@@ -15,7 +16,7 @@ for book in zp.infolist():
 
 	#titleCountInBook = 0
 	lineCount = 0
-	#titleFound = False
+	titleFound = False
 	rDateFound = False
 	dateCountInBook = 0
 	for line in data:
@@ -23,7 +24,8 @@ for book in zp.infolist():
 
 
 		if (line.find('Title:')!= -1 or line.find('Tile:')!= -1) and lineCount < 100:
-			title = line.split(':')[1:]
+			title = line.split(':')[1].strip()
+			#print "title ->"+title
 			titleCount = titleCount + 1
 			#titleCountInBook = titleCountInBook+1
 			#print line
@@ -33,6 +35,8 @@ for book in zp.infolist():
 
 		if line.find('Author:')!=-1 and lineCount < 100:
 
+			author = line.split(':')[1].strip()
+			#print "author ->"+author
 			authorCount = authorCount + 1
 
 		'''if line.find('Release Date:')!= -1 and lineCount < 100:
@@ -45,6 +49,11 @@ for book in zp.infolist():
 '''
 		if line.lower().find('[etext')!= -1 and lineCount < 100 and rDateFound == False:
 
+			rdate = line.lower().split('[etext')[0]
+			if ':' in rdate:
+				rdate = rdate.split(':')[1]
+			rdate = rdate.strip()
+			#print rdate
 			releaseDateCount = releaseDateCount + 1
 			rDateFound = True
 			dateCountInBook = dateCountInBook + 1
@@ -53,16 +62,33 @@ for book in zp.infolist():
 
 		elif line.lower().find('[ebook')!= -1 and lineCount < 100 and rDateFound == False:
 
+			rdate = line.lower().split('[ebook')[0]
+			if ':' in rdate:
+				rdate = rdate.split(':')[1]
+			rdate = rdate.strip()
+			#print rdate
 			releaseDateCount = releaseDateCount + 1
 			rDateFound = True
 			dateCountInBook = dateCountInBook + 1
 			if dateCountInBook > 1:
 				bookName = bookName+book.filename+" "
+
+		if line.find('Language:')!=-1 and lineCount < 100:
+
+			#titleFound = True
+			language = line.split(':')[1].strip()
+			print language
+			languageCount = languageCount+1
+
+
 	#if titleFound == False:
 	#	bookName = bookName+book.filename+" "
 	#if rDateFound == False:
 	#	bookName = bookName+book.filename+" "
 	bookCount = bookCount+1
+
+	# reading whole book
+	wholeBook = openBook.read()
 
 
 
@@ -70,4 +96,5 @@ print "total no of files in zip " +str(bookCount)
 print "total no of titles of books "+str(titleCount)
 print "total no of authors of books "+str(authorCount)
 print "total no of release dates found "+str(releaseDateCount)
+print "total no of languages found "+str(languageCount)
 print "these are the irregular books "+bookName
